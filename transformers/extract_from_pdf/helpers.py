0ab99470ab08bbec6dea1d5b2b715848c2be0fcd
mcascone <max@bitovi.com>
2024-05-07 14:32:44 -0500

    use page_content in helper

--- transformers/extract_from_pdf/helpers.py
+++ transformers/extract_from_pdf/helpers.py
@@ -27,7 +27,7 @@ def extract_text_from_pdf(input_dir, output_dir, file, loader_class, output_file
     output_file_path = os.path.join(output_dir, file + output_file_extension)
     with open(output_file_path, "w") as f:
         for line in lines:
-            f.write(line + "\n")
+            f.write(line.page_content + "\n")
         print(f"Text extracted from {file}")
 
 # def run_transformer(script_name, config, step, input_dir, output_dir, loader_class, output_file_extension):
